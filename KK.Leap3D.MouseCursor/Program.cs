062b9c2fae1ef2ce7c9c23cedf136687a87765cc
kkurni <kkurni@gmail.com>
2013-02-11 18:47:51 +1100

    initial test to move cursor position

---
+++ KK.Leap3D.MouseCursor/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KK.Leap3D.MouseCursor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MoveCursor();
+
+        }
+
+        private static void MoveCursor()
+        {
+            if (Cursor.Current != null)
+            {
+                var cursor = new Cursor(Cursor.Current.Handle);
+            }
+            Console.WriteLine("Move Cursor");
+            Cursor.Position = new Point(Cursor.Position.X - 50, Cursor.Position.Y);
+        }
+    }
+}
